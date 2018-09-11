cdfb0ef4db20e553adaa53671f36ea2dab8a7e8d
Dean Wu <dean.wu55@gmail.com>
2018-09-10 22:31:27 -0400

    actually fix .gitignore

---
+++ public/javascripts/modules/autocomplete.js
@@ -0,0 +1,22 @@
+function autocomplete(input, latInput, lngInput) {
+	// console.log(input, latInput, lngInput);
+	if (!input) return; // skip this fn from running if there's no input
+
+	const dropdown = new google.maps.places.Autocomplete(input);
+
+	dropdown.addListener('place_changed', () => {
+		const place = dropdown.getPlace();
+		// console.log(place);
+		latInput.value = place.geometry.location.lat();
+		lngInput.value = place.geometry.location.lng();
+	});
+
+	// if someone hits enter on the address field, don't submit the form
+	input.on('keydown', (e) => {
+		if (e.keyCode === 13) e.preventDefault();
+	});
+
+	// what if someone changes the lat and lng instead of the input???
+}
+ 
+export default autocomplete;

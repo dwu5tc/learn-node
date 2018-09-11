38e3e3b16b79927d17d0fe71da4684cbc0c9f9eb
Dean Wu <dean.wu55@gmail.com>
2018-09-10 22:25:11 -0400

    fix .gitignore

--- public/javascripts/delicious-app.js
+++
@@ -1,8 +0,0 @@
-import '../sass/style.scss';
-
-import { $, $$ } from './modules/bling';
-import autocomplete from './modules/autocomplete';
-
-// autocomplete( document.querySelector('#address'), document.querySelector('#lat'), ocument.querySelector('#lng'));
-autocomplete( $('#address'), $('#lat'), $('#lng') );
-

50674f8d2436fa0a6156628361c46ef759dbc9f1
Dean Wu <dean.wu55@gmail.com>
2018-09-23 15:20:23 -0400

    complete l36 displaying our hearted stores

--- routes/index.js
+++ routes/index.js
@@ -65,6 +65,9 @@ router.post('/account/reset/:token',
   authController.confirmedPasswords,
   catchErrors(authController.update));
 router.get('/map', storeController.mapPage);
+router.get('/hearts', 
+  authController.isLoggedIn,
+  catchErrors(storeController.getHearts));
 
 // API
 router.get('/api/search', catchErrors(storeController.searchStores));
